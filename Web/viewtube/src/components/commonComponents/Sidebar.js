19efc870be01f92a2a70306679bd09488934557b
YuvalMaaravi <yuvalm1998@gmail.com>
2024-05-30 14:05:26 +0300

    add the basics of the sidebar

--- Web/viewtube/src/components/commonComponents/Sidebar.js
+++ Web/viewtube/src/components/commonComponents/Sidebar.js
@@ -1,9 +1,17 @@
 import React from "react";
 import './Sidebar.css';
+import home_icon from '../../assets/home.png'
 
 const Sidebar = () => {
     return (
-       
+       <div className='sidebar'>
+        <div className='shortcut-links'>
+            <div className='side-links'>
+                <img src={home_icon}/><p>Home</p>
+            </div>
+        </div>
+
+       </div>
 
     )
 }
--- Web/viewtube/src/pages/Home.js
+++ Web/viewtube/src/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import VideoList from '../components/homePage/VideoList';
 import Navbar from '../components/commonComponents/Navbar';
 import styles from './Home.css';
+import Sidebar from '../components/commonComponents/Sidebar';
 
 const Home = () => {
   return (
     <div className={styles.homePage}>
       <Navbar />
+      <Sidebar />
       <VideoList />
     </div>
   );
