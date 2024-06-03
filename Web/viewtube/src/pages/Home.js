67ed0a62be1f63fb8a6efc43f4b185ce8ac8cf8c
YuvalMaaravi <yuvalm1998@gmail.com>
2024-06-03 12:43:43 +0300

    merging home.js

--- Web/viewtube/src/pages/Home.js
+++ Web/viewtube/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import VideoList from '../components/homePage/VideoList';
 import Navbar from '../components/commonComponents/Navbar';
-<<<<<<< sidebar-branch
+
 import styles from './Home.css';
 import Sidebar from '../components/commonComponents/Sidebar';
-=======
 import './Home.css';
->>>>>>> main
 
 const Home = () => {
   return (
