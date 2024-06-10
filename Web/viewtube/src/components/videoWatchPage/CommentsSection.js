47de3b2da16e714e02bd1dd81a541ad34ae09691
ZivElbaz <zivel111@gmail.com>
2024-06-10 17:22:28 +0300

    finished editing and deleting the comments

--- Web/viewtube/src/components/videoWatchPage/CommentsSection.js
+++ Web/viewtube/src/components/videoWatchPage/CommentsSection.js
@@ -1,15 +1,20 @@
-// src/components/videoWatchPage/CommentsSection.js
 import React, { useState } from 'react';
 import './CommentsSection.css';
 import Profile from '../../assets/profile-circle.jpg';
+import editIcon from '../../assets/edit_icon.svg';
+import deleteIcon from '../../assets/delete_icon.svg';
+import saveIcon from '../../assets/tick_icon.svg';
+import cancelIcon from '../../assets/cancel_icon.svg';
 import { useComments } from '../../contexts/CommentsContext';
 import { useUser } from '../../contexts/UserContext';
 
 function CommentsSection({ videoId }) {
-  const { comments, addComment, getCommentsByVideoId } = useComments();
+  const { comments, addComment, updateComment, deleteComment, getCommentsByVideoId } = useComments();
   const { currentUser } = useUser();
   const videoComments = getCommentsByVideoId(videoId);
   const [newComment, setNewComment] = useState('');
+  const [editingCommentId, setEditingCommentId] = useState(null);
+  const [editCommentText, setEditCommentText] = useState('');
 
   const handleCommentSubmit = (e) => {
     e.preventDefault();
@@ -21,6 +26,22 @@ function CommentsSection({ videoId }) {
     };
     addComment(comment);
     setNewComment('');
+  };
+
+  const handleEditComment = (comment) => {
+    setEditingCommentId(comment.id);
+    setEditCommentText(comment.text);
+  };
+
+  const handleSaveEdit = (id) => {
+    updateComment(id, editCommentText);
+    setEditingCommentId(null);
+    setEditCommentText('');
+  };
+
+  const handleCancelEdit = () => {
+    setEditingCommentId(null);
+    setEditCommentText('');
   };
 
   return (
@@ -45,8 +66,46 @@ function CommentsSection({ videoId }) {
       </div>
       <ul>
         {videoComments.map((comment) => (
-          <li key={comment.id}>
-            <strong>{comment.author}</strong>: {comment.text}
+          <li key={comment.id} className="comment-item">
+            {editingCommentId === comment.id ? (
+              <>
+                <div className="comment-text">
+                  <strong>{comment.author}</strong>:
+                </div>
+                <input
+                  type="text"
+                  value={editCommentText}
+                  onChange={(e) => setEditCommentText(e.target.value)}
+                  className="edit-input"
+                />
+                <div className="comment-actions">
+                  <button className="save-button action-button" onClick={() => handleSaveEdit(comment.id)}>
+                    <img src={saveIcon} alt="Save" width="16" height="16"/>
+                  </button>
+                  <button className="cancel-button action-button" onClick={handleCancelEdit}>
+                    <img src={cancelIcon} alt="Cancel" width="16" height="16"/>
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="comment-text">
+                  <strong>{comment.author}</strong>: {comment.text}
+                </div>
+                <div className="comment-actions">
+                  {currentUser && currentUser.username === comment.author && (
+                    <>
+                      <button className="edit-button action-button" onClick={() => handleEditComment(comment)}>
+                        <img src={editIcon} alt="Edit" width="16" height="16"/>
+                      </button>
+                      <button className="delete-button action-button" onClick={() => deleteComment(comment.id)}>
+                        <img src={deleteIcon} alt="Delete" width="16" height="16"/>
+                      </button>
+                    </>
+                  )}
+                </div>
+              </>
+            )}
           </li>
         ))}
       </ul>
